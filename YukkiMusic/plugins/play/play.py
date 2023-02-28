d32dd4c7d563eacb8ef383b0e11e4d5dbd49621a
D3NE <122222544+D3NE@users.noreply.github.com>
2023-02-28 22:33:36 +0300

    Update play.py

--- YukkiMusic/plugins/play/play.py
+++ YukkiMusic/plugins/play/play.py
@@ -39,7 +39,7 @@ from YukkiMusic.utils.stream.stream import stream
 PLAY_COMMAND = get_command("PLAY_COMMAND")
 
 
-@app.on_message(
+@Client.on_message(
     filters.command(PLAY_COMMAND)
     & filters.group
     & ~filters.edited
